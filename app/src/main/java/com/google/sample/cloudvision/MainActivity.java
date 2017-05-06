f745a0bdb302c9540f9bc87de26dc63f54d55cb3
ishaquehassan <ishaquehassan@gmail.com>
2017-05-06 16:43:23 +0500

    Just Created

--- app/src/main/java/com/google/sample/cloudvision/MainActivity.java
+++ app/src/main/java/com/google/sample/cloudvision/MainActivity.java
@@ -64,7 +64,7 @@ import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String CLOUD_VISION_API_KEY = "AIzaSyBgj_xLm294B8SqaPPCSquURxV2aYRVC8g";
+    private static final String CLOUD_VISION_API_KEY = "API KEY";
     private static final String FILE_NAME = "temp.jpg";
     private static final String ANDROID_CERT_HEADER = "X-Android-Cert";
     private static final String ANDROID_PACKAGE_HEADER = "X-Android-Package";
